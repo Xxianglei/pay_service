f411a358e142fe2a02097138443e5c0e53bf05ea
BB  zebra <1767925069@qq.com>
2019-11-25 23:51:08 +0800

    Update ProductServiceApplication.java

--- src/main/java/com/xianglei/charge_service/ProductServiceApplication.java
+++ src/main/java/com/xianglei/charge_service/ProductServiceApplication.java
@@ -2,8 +2,12 @@ package com.xianglei.charge_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
