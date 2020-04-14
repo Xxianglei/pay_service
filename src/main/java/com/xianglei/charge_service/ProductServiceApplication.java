0852dc2c828f98543ae83b2108306271e10f1fc0
xianglei <xianglei@sefonsoft.com>
2020-04-14 13:23:30 +0800

    修改注册中心 nacos

--- src/main/java/com/xianglei/charge_service/ProductServiceApplication.java
+++ src/main/java/com/xianglei/charge_service/ProductServiceApplication.java
@@ -2,10 +2,12 @@ package com.xianglei.charge_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableFeignClients
+@EnableDiscoveryClient
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
