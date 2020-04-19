eb80ca75cc620f4a892b53eda07a79f406769a70
xianglei <xianglei@sefonsoft.com>
2020-04-19 10:49:49 +0800

    .json 后缀

--- src/main/java/com/xianglei/charge_service/PayOrderServiceApplication.java
+++ src/main/java/com/xianglei/charge_service/PayOrderServiceApplication.java
@@ -3,8 +3,11 @@ package com.xianglei.charge_service;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.DispatcherServlet;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -14,5 +17,16 @@ public class PayOrderServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PayOrderServiceApplication.class, args);
+    }
+    /* * 设置匹配*.json后缀请求
+     * @param dispatcherServlet
+     * @return-该设置严格指定匹配后缀*.do或.action，但有风险 可能不生效
+     *
+     **/
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean(DispatcherServlet dispatcherServlet) {
+        ServletRegistrationBean<DispatcherServlet> servletServletRegistrationBean = new ServletRegistrationBean<>(dispatcherServlet);
+        servletServletRegistrationBean.addUrlMappings("*.json");
+        return servletServletRegistrationBean;
     }
 }
